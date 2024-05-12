2dd0f9b65d3bd03856bbf4c68b93068e7b067753
Isaiah Marc Sanchez <2328269+imsanchez@users.noreply.github.com>
2024-05-12 13:48:17 -0500

    Initial Setup (#1)

---
+++ .github/pull_request_template.md
@@ -0,0 +1,10 @@
+Review Level: Glance | Normal | Deep
+
+<!--
+# Description
+
+## Additional Details
+
+# Motivation
+
+-->
---
+++ packages/next/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, type Options } from "tsup";
+
+export default defineConfig((options: Options) => ({
+  clean: process.env.NODE_ENV === "production",
+  dts: false, // for DX performance
+  entry: ["src"],
+  format: ["esm", "cjs"],
+  minify: process.env.NODE_ENV === "production",
+  platform: "neutral",
+  replaceNodeEnv: true,
+  sourcemap: process.env.NODE_ENV === "development",
+  splitting: true,
+  target: "es5",
+  ...options,
+}));
