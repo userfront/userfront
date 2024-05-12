d2105f75ab1bc96005e703f8eef406f85ef75430
Isaiah Marc Sanchez <2328269+imsanchez@users.noreply.github.com>
2024-05-12 14:03:35 -0500

    Eslint Configs (#2)

---
+++ packages/next/eslint.config.mjs
@@ -0,0 +1,28 @@
+export default [
+  {
+      ignores: [
+        ".git/",
+        "**/.git/",
+        "dist/",
+        "**/dist/",
+        "build/",
+        "**/build/",
+        "public/",
+        "**/public/",
+        ".next/",
+        "**/.next/",
+        "coverage/",
+        "**/coverage/",
+        ".vercel/",
+        "**/.vercel/",
+        "node_modules/",
+        "**/node_modules/"
+      ]
+  },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error"
+    }
+  }
+];
