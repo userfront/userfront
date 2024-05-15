990738763b5d804d3352daa2fdccd41fd2a4d0a4
Isaiah Marc Sanchez <2328269+imsanchez@users.noreply.github.com>
2024-05-15 01:10:00 -0500

    add dummy unit test, for now

--- packages/react/src/index.test.ts
+++ packages/react/src/index.test.ts
@@ -1,8 +1,5 @@
-// import { expect, test } from "vitest";
-// import { sum } from "./index";
-//
-// test("adds 1 + 2 to equal 3", () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+import { expect, test } from "vitest";
 
-export {};
+test("1 is 1", () => {
+  expect(1).toBe(1);
+});
